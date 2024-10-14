23a7cbde81a2f1ec7776336c667e7ee059e4d11c
yotsubo <513047.otsubo@gmail.com>
2024-10-14 11:33:13 +0900

    ref: pipex.h: add comment

--- pipex.h
+++ pipex.h
@@ -20,9 +20,11 @@
 # include <sys/wait.h>
 # include <errno.h>
 
+// t_bool用のマクロ
 # define TRUE 1
 # define FALSE 0
 
+// exit_status用のマクロ
 # define CMD_NOT_FOUND -1
 # define CMD_CANT_EXEC -2
 # define CMD_CANT_EXEC_STS 126
@@ -30,6 +32,9 @@
 
 typedef int	t_bool;
 
+// t_data: 各プロセスに渡す構造体
+// cmds: cmds[n][0]はコマンド名(ls, catなど), cmds[n][1]以降にはオプションなど(-aなど)。execve()で使用するものを格納。
+// cmd_paths: コマンドのpathを格納(cmds[0]に/usr/bin/ls, cmds[1]に/usr/bin/catなど)。同じくexecve()で使用する。
 typedef struct s_data {
 	char	***cmds;
 	char	**cmd_paths;
