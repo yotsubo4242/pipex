194d055f3ef0d7bf6ad29571a117ec7480f729ce
yotsubo <513047.otsubo@gmail.com>
2024-10-14 11:33:32 +0900

    ref: main.c: add comment

--- src/main.c
+++ src/main.c
@@ -13,6 +13,9 @@
 #include "pipex.h"
 #include "libft.h"
 
+// プログラムのメインとなるファイル：main.c, data_init.c, do_cmds.c, search_cmd_path.c
+// それらの補助となるファイル：error_message.c, free_utils.c, search_cmd_path_utils.c
+
 static int	err_return(t_data *data)
 {
 	if (data)
@@ -24,10 +27,13 @@ int	main(int argc, char *argv[])
 {
 	t_data	*data;
 
+	// inputから構造体の初期化
 	data = data_init(argc, argv);
 	if (!data)
 		return (err_return(NULL));
+	// コマンドの実行
 	do_cmds(data, argv);
+	// cleanup
 	free_data(data);
 	return (0);
 }
